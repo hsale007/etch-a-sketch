82506eedb85ad93168c3249d914d87a5e4fef8bf
Hamza <199155278+hsale007@users.noreply.github.com>
2025-03-24 14:12:05 -0400

    Hover effect added to grid squares

--- scripts/script.js
+++ scripts/script.js
@@ -9,6 +9,9 @@ for (let i = 0; i < columns; i++) {
   for (let j = 0; j < rows; j++) {
     const row = document.createElement("div");
     row.className = "row";
+    row.addEventListener("mouseover", () => {
+      row.classList.toggle("coloredBox");
+    });
     column.appendChild(row);
   }
   container.appendChild(column);
