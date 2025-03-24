a82237d8d62d299a0ac0937b5491b0c4bd839a0f
Hamza <199155278+hsale007@users.noreply.github.com>
2025-03-24 17:26:33 -0400

    Fix buttons

--- scripts/script.js
+++ scripts/script.js
@@ -53,3 +53,11 @@ userInput.addEventListener("keyup", (e) => {
     container.replaceChildren();
   }
 });
+
+const buttons = document.querySelectorAll("button");
+
+buttons.forEach((button) => {
+  button.addEventListener("click", () => {
+    console.log(button.className);
+  });
+});
