6805f31371de48f9b80ab0d2c829b7aefb7b2939
Hamza <199155278+hsale007@users.noreply.github.com>
2025-03-24 16:17:56 -0400

    User user input for grid size
    Ask user what the grid size should be. Validate that the user has
    entered a number between 1-99.
    
    Grid is create any time user enters a valid input automatically. Grid
    is cleared on value change. Grid is empty if user enters invalid grid
    size.

--- scripts/script.js
+++ scripts/script.js
@@ -1,18 +1,39 @@
-let columns = 16;
-let rows = 16;
-
 const container = document.querySelector(".container");
 
-for (let i = 0; i < columns; i++) {
-  const column = document.createElement("div");
-  column.className = "columns";
-  for (let j = 0; j < rows; j++) {
-    const row = document.createElement("div");
-    row.className = "row";
-    row.addEventListener("mouseover", () => {
-      row.classList.toggle("coloredBox");
-    });
-    column.appendChild(row);
+function createGrid(gridSize) {
+  container.replaceChildren();
+  for (let i = 0; i < gridSize; i++) {
+    const column = document.createElement("div");
+    column.className = "columns";
+    for (let j = 0; j < gridSize; j++) {
+      const row = document.createElement("div");
+      row.className = "row";
+      row.addEventListener("mouseover", () => {
+        row.classList.toggle("coloredBox");
+      });
+      column.appendChild(row);
+    }
+    container.appendChild(column);
   }
-  container.appendChild(column);
 }
+
+function validateUserInput(userInput) {
+  userInput = parseInt(userInput);
+  return !isNaN(userInput) && userInput > 0 && userInput < 100;
+}
+const userInput = document.querySelector("input");
+
+//default grid size
+userInput.value = 16;
+createGrid(userInput.value);
+
+userInput.addEventListener("keyup", (e) => {
+  console.log(validateUserInput(userInput.value));
+  if (validateUserInput(userInput.value)) {
+    userInput.setCustomValidity("");
+    createGrid(userInput.value);
+  } else {
+    userInput.setCustomValidity("Yo this is wrong");
+    container.replaceChildren();
+  }
+});
